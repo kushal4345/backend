d04cc7e732d2f8761cbb8304b2f8c059dc02a485
Lavkush <college.studying23@gmail.com>
2025-01-27 18:38:12 +0530

    Work in progress..

---
+++ Frontend/Athelet/src/Component/Login.jsx
@@ -0,0 +1,12 @@
+export function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <input type="text" placeholder="Username"/>
+            <br />
+            <br />
+            <input type="text" placeholder="Password"/>
+        </div>
+    )
+}
+
